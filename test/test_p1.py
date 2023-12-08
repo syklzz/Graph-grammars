65ea1dbb54aec563465ef7dcb4b4fcbc553266dc
Agnieszka Szynalik <aszynali@akamai.com>
2023-12-08 17:45:59 +0100

    Update test case

--- test/test_p1.py
+++ test/test_p1.py
@@ -18,7 +18,7 @@ def prepare_left_side_of_production():
     graph.add_edge(Edge(node1, node2, 0))
     graph.add_edge(Edge(node2, node3, 0))
     graph.add_edge(Edge(node3, node4, 0))
-    graph.add_edge(Edge(node4, node1, 0))
+    graph.add_edge(Edge(node4, node1, 1))
 
     graph.add_square(Square([node1, node2, node3, node4], 1))
 
