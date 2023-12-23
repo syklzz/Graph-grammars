ba2844c4dfe0a53348342518dd25d05892726ff0
mrzysztof <krzysztof.maliszewski9@wp.pl>
2023-12-23 11:36:20 +0100

    graph corrections

--- graphs/graphs_p8.py
+++ graphs/graphs_p8.py
@@ -7,9 +7,9 @@ def left_side_of_p8():
     graph = Graph()
 
     node1 = Node(0, 0, 0)
-    node2 = Node(0, 2, 0)
+    node2 = Node(2, 0, 0)
     node3 = Node(2, 2, 0)
-    node4 = Node(2, 0, 0)
+    node4 = Node(0, 2, 0)
     node5 = Node(2, 1, 1)
     node6 = Node(4, 1, 0)
     node7 = Node(4, 2, 0)
@@ -24,13 +24,34 @@ def left_side_of_p8():
     return graph
 
 
+def mod_left_side_of_p8():
+    graph = Graph()
+
+    node1 = Node(0, 0, 0)
+    node2 = Node(2, 0, 0)
+    node3 = Node(2, 2, 0)
+    node4 = Node(0, 2, 0)
+    node5 = Node(1, 2, 1)
+    node6 = Node(0, 3, 0)
+    node7 = Node(1, 3, 0)
+    graph.add_nodes([node1, node2, node3, node4, node5, node6, node7])
+
+    graph.add_edge(Edge(node4, node5, 0))
+    graph.add_edge(Edge(node3, node4, 0))
+
+    graph.add_hyper_edge(HyperEdge([node1, node2, node3, node4], 0, Label.Q))
+    graph.add_hyper_edge(HyperEdge([node4, node7, node5, node6], 1, Label.Q))
+
+    return graph
+
+
 def complex_graph_with_match():
     graph = Graph()
 
     node1 = Node(0, 0, 0)
-    node2 = Node(0, 2, 0)
+    node2 = Node(2, 0, 0)
     node3 = Node(2, 2, 0)
-    node4 = Node(2, 0, 0)
+    node4 = Node(0, 2, 0)
     node5 = Node(2, 1, 1)
     node6 = Node(4, 1, 0)
     node7 = Node(4, 2, 0)
@@ -74,9 +95,9 @@ def graph_with_incorrect_hanging_node_attribute():
     graph = Graph()
 
     node1 = Node(0, 0, 0)
-    node2 = Node(0, 2, 0)
+    node2 = Node(2, 0, 0)
     node3 = Node(2, 2, 0)
-    node4 = Node(2, 0, 0)
+    node4 = Node(0, 2, 0)
     node5 = Node(2, 1, 37)
     node6 = Node(4, 1, 0)
     node7 = Node(4, 2, 0)
@@ -95,9 +116,9 @@ def graph_with_incorrect_hyper_edge_attribute():
     graph = Graph()
 
     node1 = Node(0, 0, 0)
-    node2 = Node(0, 2, 0)
+    node2 = Node(2, 0, 0)
     node3 = Node(2, 2, 0)
-    node4 = Node(2, 0, 0)
+    node4 = Node(0, 2, 0)
     node5 = Node(2, 1, 1)
     node6 = Node(4, 1, 0)
     node7 = Node(4, 2, 0)
@@ -108,26 +129,5 @@ def graph_with_incorrect_hyper_edge_attribute():
 
     graph.add_hyper_edge(HyperEdge([node1, node2, node3, node4], 42, Label.Q))
     graph.add_hyper_edge(HyperEdge([node3, node7, node5, node6], 0, Label.Q))
-
-    return graph
-
-
-def graph_with_incorrect_hyper_edge_label():
-    graph = Graph()
-
-    node1 = Node(0, 0, 0)
-    node2 = Node(0, 2, 0)
-    node3 = Node(2, 2, 0)
-    node4 = Node(2, 0, 0)
-    node5 = Node(2, 1, 1)
-    node6 = Node(4, 1, 0)
-    node7 = Node(4, 2, 0)
-    graph.add_nodes([node1, node2, node3, node4, node5, node6, node7])
-
-    graph.add_edge(Edge(node2, node5, 0))
-    graph.add_edge(Edge(node3, node5, 0))
-
-    graph.add_hyper_edge(HyperEdge([node1, node2, node3, node4], 0, "x"))
-    graph.add_hyper_edge(HyperEdge([node3, node7, node5, node6], 1, "D"))
 
     return graph
--- tests/test_p8.py
+++ tests/test_p8.py
@@ -3,13 +3,12 @@ import unittest
 from model.graph import Graph
 from production.p8 import p8
 from graphs.graphs_p8 import complex_graph_with_match, graph_with_no_match, graph_with_incorrect_hyper_edge_attribute, \
-    graph_with_incorrect_hyper_edge_label, \
-    graph_with_incorrect_hanging_node_attribute, left_side_of_p8
+    graph_with_incorrect_hanging_node_attribute, left_side_of_p8, mod_left_side_of_p8
 
 
 class TestP8(unittest.TestCase):
 
-    def test_for_left_side_of_p2(self):
+    def test_for_left_side_of_p8(self):
         expected_nodes_size = 7
         expected_edges_size = 2
         expected_hyper_edges_size = 2
@@ -17,6 +16,24 @@ class TestP8(unittest.TestCase):
         expected_r2 = 1
 
         graph = left_side_of_p8()
+        graph.draw_graph()
+        p8(graph)
+        graph.draw_graph()
+
+        self.assertTrue(len(graph.nodes) == expected_nodes_size)
+        self.assertTrue(len(graph.edges) == expected_edges_size)
+        self.assertTrue(len(graph.hyper_edges) == expected_hyper_edges_size)
+        self.assertTrue(graph.hyper_edges[0].r == expected_r1)
+        self.assertTrue(graph.hyper_edges[1].r == expected_r2)
+
+    def test_for_mod_left_side_of_p8(self):
+        expected_nodes_size = 7
+        expected_edges_size = 2
+        expected_hyper_edges_size = 2
+        expected_r1 = 1
+        expected_r2 = 1
+
+        graph = mod_left_side_of_p8()
         graph.draw_graph()
         p8(graph)
         graph.draw_graph()
@@ -101,24 +118,6 @@ class TestP8(unittest.TestCase):
         expected_r2 = 1
 
         graph = graph_with_incorrect_hyper_edge_attribute()
-        graph.draw_graph()
-        p8(graph)
-        graph.draw_graph()
-
-        self.assertTrue(len(graph.nodes) == expected_nodes_size)
-        self.assertTrue(len(graph.edges) == expected_edges_size)
-        self.assertTrue(len(graph.hyper_edges) == expected_hyper_edges_size)
-        self.assertTrue(graph.hyper_edges[0].r == expected_r1)
-        self.assertTrue(graph.hyper_edges[1].r == expected_r2)
-
-    def test_for_graph_with_incorrect_hyper_edge_label(self):
-        expected_nodes_size = 7
-        expected_edges_size = 2
-        expected_hyper_edges_size = 2
-        expected_r1 = 0
-        expected_r2 = 1
-
-        graph = graph_with_incorrect_hyper_edge_label()
         graph.draw_graph()
         p8(graph)
         graph.draw_graph()
