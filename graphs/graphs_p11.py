9334d20cb4154013da7fee379cd54586008b9343
Ehevi <aga.cyra@wp.pl>
2024-01-11 20:02:03 +0100

    Cleanup :broom:
    Co-authored-by: Ehevi <48785655+Ehevi@users.noreply.github.com>
    Co-authored-by: Paweł Kruczkiewicz <pawel.kruczkiewicz@gmail.com>

--- graphs/graphs_p11.py
+++ graphs/graphs_p11.py
@@ -225,8 +225,7 @@ if __name__=="__main__":
     print("This module is not meant to be executed directly.")
     print("But if you insist, here is a demo:")
     
-    for graph in [#exact_left_side()]: #, 
-        complex_graph_with_match()]:#, graph_with_no_match(), graph_wrong_label(), graph_wrong_hanging_node(), graph_wrong_r()]:
+    for graph in [exact_left_side(), complex_graph_with_match(), graph_with_no_match(), graph_wrong_label(), graph_wrong_hanging_node(), graph_wrong_r()]:
         graph.draw_graph()
         p11(graph)
         graph.draw_graph()
