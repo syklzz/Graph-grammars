214b7e3cbfb5a473b991a91d0fa387485bc1a077
mrzysztof <krzysztof.maliszewski9@wp.pl>
2024-01-17 19:51:57 +0100

    parameterize p7

--- production/p7.py
+++ production/p7.py
@@ -1,20 +1,23 @@
-from typing import Optional, Tuple
+from typing import Optional, List
 from model.graph import Graph
 from model.edge import Edge, Label
 
-
-def p7(graph: Graph, coordinates: Optional[Tuple[int, int, int]] = None) -> None:
+def p7(graph: Graph, ids: Optional[List[int]] = None) -> None:
     """
-    Virtual adaptation: Marking the four-edged element to be broken
+    Virtual adaptation: Marking the four-edged element to be broken.
+    If a list of ids is provided, only hyperedges whose nodes' ids match the provided ids are considered.
+    
+    Parameters:
+    graph (Graph): The graph on which the operation is performed.
+    ids (List[int], optional): List of node ids to consider. If None, all nodes are considered. Defaults to None.
     """
     for hyper_edge in graph.hyper_edges:
-        if coordinates is not None:
-            if not any(node.x == coordinates[0] and node.y == coordinates[1] and node.h == coordinates[2] for node in hyper_edge.nodes):
+        if ids is not None:
+            if not all(node.id in ids for node in hyper_edge.nodes):
                 continue
         if _should_break(hyper_edge):
             hyper_edge.r = 1
             return
-
 
 def _should_break(edge: Edge) -> bool:
     return edge.label == Label.Q and len(edge.nodes) == 4 and edge.r == 0
