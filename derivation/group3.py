b4881b0ef2e504796728930a828f71e43fb53e68
mrzysztof <krzysztof.maliszewski9@wp.pl>
2024-01-17 19:51:57 +0100

    parameterize p7

--- derivation/group3.py
+++ derivation/group3.py
@@ -1,6 +1,14 @@
 from graphs import derive_base
-
+from graphs.derive_base import derive_base
+from production.p1 import p1
+from production.p7 import p7
 
 def derive():
     g = derive_base()
-    g.add_hyperedge
+    p7(g)
+    p1(g)
+    p7(g)
+    g.draw_graph()
+
+if __name__ == '__main__':
+    derive()
