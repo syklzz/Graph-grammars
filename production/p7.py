93eb02d99d303182a5ef19b8a73309f2748133c9
robertkbk <33332309+robertkbk@users.noreply.github.com>
2024-01-24 18:30:00 +0100

    Fixed issues with productions

--- production/p7.py
+++ production/p7.py
@@ -2,11 +2,12 @@ from typing import Optional, List
 from model.graph import Graph
 from model.edge import Edge, Label
 
+
 def p7(graph: Graph, ids: Optional[List[int]] = None) -> None:
     """
     Virtual adaptation: Marking the four-edged element to be broken.
     If a list of ids is provided, only hyperedges whose nodes' ids match the provided ids are considered.
-    
+
     Parameters:
     graph (Graph): The graph on which the operation is performed.
     ids (List[int], optional): List of node ids to consider. If None, all nodes are considered. Defaults to None.
@@ -18,6 +19,7 @@ def p7(graph: Graph, ids: Optional[List[int]] = None) -> None:
         if _should_break(hyper_edge):
             hyper_edge.r = 1
             return
+
 
 def _should_break(edge: Edge) -> bool:
     return edge.label == Label.Q and len(edge.nodes) == 4 and edge.r == 0
