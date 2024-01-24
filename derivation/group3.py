e7f364f857e501d50bbe4f7355fdc9d24bb4b4bf
robertkbk <33332309+robertkbk@users.noreply.github.com>
2024-01-24 18:30:37 +0100

    Adjusted figures and added saving to file

--- derivation/group3.py
+++ derivation/group3.py
@@ -12,21 +12,23 @@ from production.p17 import p17
 
 def derive():
     g = derive_base()
-    # g.draw_graph()
+
+    g.draw_graph("d3-0-init.png")
     p7(g)
-    # g.draw_graph()
+    g.draw_graph("d3-1-p7.png")
     p1(g)
-    # g.draw_graph()
+    g.draw_graph("d3-2-p1.png")
     p7(g, [8, 10, 14, 13])
-    # g.draw_graph()
+    g.draw_graph("d3-3-p7.png")
     p8(g)
-    g.draw_graph()
-    p17(g)  # Bug with P17: renames nodes
-    g.draw_graph()
+    g.draw_graph("d3-4-p8.png")
+    # Bug with P17: renames nodes
+    p17(g)
+    g.draw_graph("d3-5-p17.png")
     p10(g)
-    g.draw_graph()
-    # p3(g)  # Bug with p3: circular import and does not work/end
-    # g.draw_graph()
+    g.draw_graph("d3-6-p10.png")
+    p3(g)  # Bug with p3: circular import and does not work/end
+    g.draw_graph("d3-7-p3.png")
 
 
 if __name__ == "__main__":
