4b548c4ab0da984292f53cf35092185e12a3624f
piotrkica <piotr.kica.35@gmail.com>
2024-01-26 01:28:21 +0100

    finish group3-derivation

--- derivation/group3.py
+++ derivation/group3.py
@@ -1,16 +1,50 @@
 # from graphs import derive_base
+from itertools import combinations
+
+from model.graph import Graph
+
 from graphs.derive_base import derive_base
 
 from production.p1 import p1
 from production.p10 import p10
 from production.p2 import p2
-
+from production.p2 import apply_production as apply_p2_production
 from production.p3 import p3
 from production.p7 import p7
 from production.p8 import p8
 from production.p17 import p17
 from model.node import Node
 from model.edge import Edge, HyperEdge, Label
+
+
+def find_hyper_edges_with_label_and_ids(graph: Graph, label: Label, ids: list[int]):
+    for hyper_edge in graph.hyper_edges:
+        if hyper_edge.label == label:
+            nodes = list(filter(lambda x: x.id in ids, hyper_edge.nodes))
+            if len(nodes) == len(ids):
+                return hyper_edge
+
+def p2(graph: Graph, p2_nodes):
+    (hanging_id, node_ids) = p2_nodes.pop(0)
+
+    hyper_edge = find_hyper_edges_with_label_and_ids(graph, Label.Q, node_ids)
+    hanging_node = next(n for n in graph.nodes if n.id == hanging_id)
+
+    nodes = []
+    for id in node_ids:
+        nodes.append(next(n for n in graph.nodes if n.id == id))
+
+    nodes.append(hanging_node)
+
+    edges = []
+    for n, m in combinations(nodes, 2):
+        try:
+            edge = next(e for e in graph.edges if ((e.n1 == n and e.n2 == m) or (e.n2 == n and e.n1 == m)))
+            edges.append(edge)
+        except StopIteration:
+            continue
+
+    apply_p2_production(graph, (hyper_edge, edges, hanging_node))
 
 
 def derive():
@@ -25,39 +59,26 @@ def derive():
     g.draw_graph("d3-3-p7.png")
     p8(g)
     g.draw_graph("d3-4-p8.png")
-    # Bug with P17: renames nodes
     p17(g)
     g.draw_graph("d3-5-p17.png")
     p10(g)
-
-    # ### ARTIFICIAL P3 PRODUCTION ###
-    g.hyper_edges.pop(2)
-    middle_node = Node(2.85, 5.25, 0)
-    g.add_node(middle_node)
-    g.add_edge(Edge(middle_node, g.nodes[19], 0))
-    g.nodes[19].h = 0
-    g.add_edge(Edge(middle_node, g.nodes[14], 0))
-    g.nodes[14].h = 0
-
-    g.add_hyper_edge(
-        HyperEdge([g.nodes[20], g.nodes[19], g.nodes[8], g.nodes[14]], 0, Label.Q))
+    g.draw_graph("d3-6-p10.png")
+    p3(g)
     g.draw_graph("d3-7-p3.png")
-
-    # p3(g)  # Bug with p3: circular import and does not work/end
-    # g.draw_graph("d3-7-p3.png")
-
-    # ### END ARTIFICIAL P3 PRODUCTION ###
-
     p1(g)
-    g.draw_graph("d3-9-p1.png")
-    p7(g, [8, 24, 21, 22])
-    g.draw_graph("d3-10-p7.png")
+    g.draw_graph("d3-8-p1.png")
+    p7(g, [8, 26, 24, 23])
+    g.draw_graph("d3-9-p7.png")
     p8(g, [8, 14, 19, 20])
-    g.draw_graph("d3-11-p8.png")
+    g.draw_graph("d3-10-p8.png")
     p8(g, [8, 13, 15, 19])
-    g.draw_graph("d3-12-p8.png")
-    # p2(g)
-    # g.draw_graph("d3-13-p2.png")
+    g.draw_graph("d3-11-p8.png")
+    p2(g, p2_nodes=[(24, [19, 15, 13, 8])])
+    g.draw_graph("d3-12-p2.png")
+    p3(g)
+    g.draw_graph("d3-13-p2.png")
+    p1(g)
+    g.draw_graph("d3-14-p2.png")
 
 
 if __name__ == "__main__":
--- production/p3.py
+++ production/p3.py
@@ -9,15 +9,16 @@ from typing import List, NamedTuple, Set
 
 from model.node import Node
 
-import functools
-
-# from graphs.graphs_p3 import *
-
 
 class Subgraph(NamedTuple):
     hyper_edge: HyperEdge
     nodes: Set[Node]
     edges: List[Edge]
+
+
+class SimpleEdge(NamedTuple):
+    n1_id: int
+    n2_id: int
 
 
 def p3(graph: Graph) -> None:
@@ -34,18 +35,77 @@ def _find_isomorphic_subgraph(graph: Graph) -> Subgraph | None:
         hyper_edge
         for hyper_edge in graph.hyper_edges
         if hyper_edge.label == Label.Q
-        and hyper_edge.r == 1
-        and len(hyper_edge.nodes) == 4
-        and all(node.h == 0 for node in hyper_edge.nodes)
+           and len(hyper_edge.nodes) == 4
+           and hyper_edge.r == 1
+           and all(node.h == 0 for node in hyper_edge.nodes)
     ]
 
+    # print("Found valid hyper edges:", valid_hyper_edges)
     subgraphs = (
         Subgraph(hyper_edge, _get_nodes_from_edges(edges), edges)
         for hyper_edge in valid_hyper_edges
-        for edges in combinations(graph.edges, 6)
+        for edges in _get_valid_edges(graph, hyper_edge.nodes)
         if _validate_subgraph(edges, hyper_edge.nodes)
     )
     return next(subgraphs, None)
+
+
+def _get_valid_edges(graph: Graph, corner_nodes: List[Node]) -> List[Edge]:
+    simple_edges = _edges_to_simple_edges(graph.edges)
+
+    corner_edges = [_get_edge(simple_edges, corner_node1, corner_node2)
+                    for corner_node1 in corner_nodes
+                    for corner_node2 in corner_nodes
+                    if corner_node1.id > corner_node2.id
+                    and _does_edge_exist(simple_edges, corner_node1, corner_node2)]
+
+    if len(corner_edges) != 2:
+        return []
+
+    count_nodes = {node: 0 for node in corner_nodes}
+    for edge in corner_edges:
+        count_nodes[edge.n1] += 1
+        count_nodes[edge.n2] += 1
+
+    nodes_with_one_edge_found = [node for node, count in count_nodes.items() if count == 1]
+    if len(nodes_with_one_edge_found) != 2:
+        return []
+
+    nodes_without_edge_found = [node for node, count in count_nodes.items() if count == 0]
+    if len(nodes_without_edge_found) != 1:
+        return []
+
+    n_a = nodes_without_edge_found[0]
+    n_b1, n_b2 = tuple(nodes_with_one_edge_found)
+
+    hanging_nodes = [n_h for n_h in _get_hanging_nodes(graph.nodes) if _does_edge_exist(simple_edges, n_h, n_a)]
+    # valid_combinations = [combination for combination in combinations(hanging_nodes, 2)
+    #                       if _does_edge_exist(simple_edges, combination[0], n_b1) or _does_edge_exist(simple_edges, combination[0], n_b2)]
+    hanging_node_edges = []
+    for n_h in hanging_nodes:
+        hanging_node_edges.append(_get_edge(simple_edges, n_h, n_a))
+        if _does_edge_exist(simple_edges, n_h, n_b1):
+            hanging_node_edges.append(_get_edge(simple_edges, n_h, n_b1))
+        elif _does_edge_exist(simple_edges, n_h, n_b2):
+            hanging_node_edges.append(_get_edge(simple_edges, n_h, n_b2))
+
+    return [corner_edges + hanging_node_edges]
+
+
+def _edges_to_simple_edges(edges: List[Edge]) -> dict[SimpleEdge, Edge]:
+    return {SimpleEdge(edge.n1.id, edge.n2.id): edge for edge in edges}
+
+
+def _does_edge_exist(edges: dict[SimpleEdge, Edge], n1: Node, n2: Node) -> bool:
+    return SimpleEdge(n1.id, n2.id) in edges or SimpleEdge(n2.id, n1.id) in edges
+
+
+def _get_edge(edges: dict[SimpleEdge, Edge], n1: Node, n2: Node) -> Edge:
+    return edges[SimpleEdge(n1.id, n2.id)] if SimpleEdge(n1.id, n2.id) in edges else edges[SimpleEdge(n2.id, n1.id)]
+
+
+def _get_hanging_nodes(nodes: list[Node]) -> list[Node]:
+    return [node for node in nodes if node.h == 1]
 
 
 def _validate_subgraph(edges: List[Edge], corner_nodes: List[Node]) -> bool:
@@ -62,7 +122,6 @@ def _validate_subgraph(edges: List[Edge], corner_nodes: List[Node]) -> bool:
     return are_nodes_in_edges and are_hanging_nodes_valid and is_isomorphic
 
 
-@functools.cache
 def _create_base_graph() -> nx.Graph:
     """Graph with four-edged element and a hyperEdge with label Q in the middle.
     The left side of P3 production. Given on slide number 12.
@@ -71,14 +130,7 @@ def _create_base_graph() -> nx.Graph:
     "hanging" nodes: 5,6
 
     """
-    corner_and_hanging_nodes_edge_list = [
-        (1, 6),
-        (6, 2),
-        (2, 5),
-        (5, 3),
-        (3, 4),
-        (4, 1),
-    ]
+    corner_and_hanging_nodes_edge_list = [(1, 6), (6, 2), (2, 5), (5, 3), (3, 4), (4, 1)]
     hyper_edge_edge_list = [(0, 1), (0, 2), (0, 3), (0, 4)]
 
     return nx.Graph(corner_and_hanging_nodes_edge_list + hyper_edge_edge_list)
@@ -126,47 +178,31 @@ def _apply_production(graph: Graph, subgraph: Subgraph) -> None:
     graph.hyper_edges.remove(hyper_edge)
 
     old_hanging_nodes = [node for node in nodes if node.h == 1]
-    old_outer_edges = [
-        edge
-        for edge in edges
-        if edge.n1 in old_hanging_nodes or edge.n2 in old_hanging_nodes
-    ]
+    old_outer_edges = [edge for edge in edges if edge.n1 in old_hanging_nodes or edge.n2 in old_hanging_nodes]
     edges_to_change = [edge for edge in edges if edge.n1.h == 0 and edge.n2.h == 0]
 
     middle_node = _create_derived_node(hyper_edge.nodes, 0)
-    new_hanging_nodes = [
-        _create_derived_node([edge.n1, edge.n2], int(not edge.b))
-        for edge in edges_to_change
-    ]
+    new_hanging_nodes = [_create_derived_node([edge.n1, edge.n2], int(not edge.b))
+                         for edge in edges_to_change]
     _remove_edges_from_graph(graph, edges_to_change)
     _add_nodes_to_graph(graph, [middle_node] + new_hanging_nodes)
 
-    new_outer_edges = [
-        outer_edge
-        for edge, h_node in zip(edges_to_change, new_hanging_nodes)
-        for outer_edge in (
-            Edge(n1=edge.n1, n2=h_node, b=edge.b),
-            Edge(n1=h_node, n2=edge.n2, b=edge.b),
-        )
-    ]
-    new_inner_edges = [
-        Edge(n1=middle_node, n2=h_node, b=0)
-        for h_node in (old_hanging_nodes + new_hanging_nodes)
-    ]
+    new_outer_edges = [outer_edge
+                       for edge, h_node in zip(edges_to_change, new_hanging_nodes)
+                       for outer_edge in
+                       (Edge(n1=edge.n1, n2=h_node, b=edge.b),
+                        Edge(n1=h_node, n2=edge.n2, b=edge.b))]
+    new_inner_edges = [Edge(n1=middle_node, n2=h_node, b=0)
+                       for h_node in (old_hanging_nodes + new_hanging_nodes)]
     _add_edges_to_graph(graph, new_outer_edges + new_inner_edges)
 
     all_subgraph_outer_edges = new_outer_edges + old_outer_edges
-    new_hyper_edges = [
-        HyperEdge(
-            nodes=(
-                [node, middle_node]
-                + _find_adjoining_nodes(node, all_subgraph_outer_edges)
-            ),
-            r=0,
-            label=Label.Q,
-        )
-        for node in hyper_edge.nodes
-    ]
+    new_hyper_edges = [HyperEdge(
+        nodes=([node, middle_node] +
+               _find_adjoining_nodes(node, all_subgraph_outer_edges)),
+        r=0,
+        label=Label.Q,
+    ) for node in hyper_edge.nodes]
     _add_hyper_edges_to_graph(graph, new_hyper_edges)
 
     for hanging_node in old_hanging_nodes:
@@ -202,37 +238,8 @@ def _remove_edges_from_graph(graph: Graph, edges: List[Edge]) -> None:
 
 
 def _find_adjoining_nodes(node: Node, edges: List[Edge]) -> List[Node]:
-    return [
-        edge.n1 if edge.n2 == node else edge.n2
-        for edge in edges
-        if node in (edge.n1, edge.n2)
-    ]
+    return [edge.n1 if edge.n2 == node else edge.n2 for edge in edges if node in (edge.n1, edge.n2)]
 
 
 def _get_nodes_from_edges(edges: List[Edge]) -> Set[Node]:
     return set([edge.n1 for edge in edges] + [edge.n2 for edge in edges])
-
-
-# if __name__ == "__main__":
-#     names = [
-#         "left side of P3",
-#         "complex graph with match",
-#         "complex graph without match",
-#         "Wrong label",
-#         "Wrong r",
-#         "Wrong h",
-#     ]
-#     test_graphs = [
-#         exact_left_side(),
-#         complex_graph_with_match(),
-#         graph_with_no_match(),
-#         graph_wrong_label(),
-#         graph_wrong_r(),
-#         graph_wrong_hanging_node(),
-#     ]
-#     for graph, name in zip(test_graphs, names):
-#         print(f"Name: {name}")
-#         graph.draw_graph()
-#         p3(graph)
-#         graph.draw_graph()
-#         print()
